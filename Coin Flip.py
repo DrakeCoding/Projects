37909e6aab77a8f9aefa98060856703765708382
DrakeCoding <106654150+DrakeCoding@users.noreply.github.com>
2022-08-04 12:32:09 -0600

    Create Coin Flip.py

---
+++ Coin Flip.py
@@ -0,0 +1,20 @@
+import random
+
+def flip(times):
+    choices = ["Heads", "Tails"]
+    heads, tails = 0, 0
+    
+    for x in range(0, times):
+        flip = random.choice(choices)
+        if flip == "Heads":
+            heads+=1
+        elif flip == "Tails":
+            tails+=1
+        
+    print(f"\nHeads:\t{heads}\nTails:\t{tails}")
+
+while True:
+    try:
+        flip(int(input("How many times would you like to flip a coin?\t")))
+    except:
+        print("Please type a valid number...\n")
